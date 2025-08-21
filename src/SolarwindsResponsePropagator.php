9075b72eb9023ba12144e0c9ad0340cf02731590
Jerry Leung <tingfung.leung@solarwinds.com>
2025-08-21 15:17:32 -0700

    Try

--- src/SolarwindsResponsePropagator.php
+++ src/SolarwindsResponsePropagator.php
@@ -48,14 +48,14 @@ final class SolarwindsResponsePropagator implements ResponsePropagator
         $header = self::SUPPORTED_VERSION . '-' . $traceId . '-' . $spanId . '-' . $samplingFlag;
         $setter->set($carrier, self::X_TRACE, $header);
 
-        $traceState = $spanContext->getTraceState();
-        if ($traceState !== null) {
-            $xtrace_options_response = $traceState->get("xtrace_options_response");
-            if ($xtrace_options_response !== null) {
-                $replaced = str_replace('....', ',', $xtrace_options_response);
-                $final = str_replace('####', '=', $replaced);
-                $setter->set($carrier, self::X_TRACE_OPTIONS_RESPONSE, $final);
-            }
-        }
+//        $traceState = $spanContext->getTraceState();
+//        if ($traceState !== null) {
+//            $xtrace_options_response = $traceState->get("xtrace_options_response");
+//            if ($xtrace_options_response !== null) {
+//                $replaced = str_replace('....', ',', $xtrace_options_response);
+//                $final = str_replace('####', '=', $replaced);
+//                $setter->set($carrier, self::X_TRACE_OPTIONS_RESPONSE, $final);
+//            }
+//        }
     }
 }
